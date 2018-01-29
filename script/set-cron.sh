d2bb164f775c739520ef7492814efab3565e25b9
Anthony Comtois <anthony.comtois@sky.uk>
2018-01-29 17:32:57 +0000

    Fix env variable for script triggered by cron

--- script/set-cron.sh
+++ script/set-cron.sh
@@ -3,6 +3,6 @@
 # default each Sunday 0:00
 cronTime="${cronTime:-0 0 * * 0}"
 
-echo "${cronTime} /root/script/cron-cleanup-docker-registry-image.sh 2>&1 > /var/log/clean-images-registry.log" > /etc/crontab
+echo "${cronTime} /bin/bash -l -c '/root/script/cron-cleanup-docker-registry-image.sh 2>&1 > /var/log/clean-images-registry.log'" > /etc/crontab
 crontab /etc/crontab
 /sbin/my_init -- tail -f /var/log/clean-images-registry.log
